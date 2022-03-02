73a4f1d0789bc4a93ca3d1cc5306a2209d907290
fhonoria <83973848+fhonoria@users.noreply.github.com>
2022-03-02 16:38:28 +0100

    build up the framework

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import "./components/Introduction";
+import Introduction from "./components/Introduction";
+import Team from "./components/Team";
+import Products from "./components/Products";
+import logo from "./images/logo.png";
+import Contact from "./components/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img
+        src={logo}
+        alt="Thermal delikat logo"
+        className="img-fluid w-25"
+      ></img>
+      <h1>Thermál Delikát</h1>
+      <Introduction />
+      <Team />
+      <Products />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
