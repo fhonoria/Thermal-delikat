e0afce83a5e3694db6dda83b235f5ce158ae36c3
fhonoria <83973848+fhonoria@users.noreply.github.com>
2022-03-18 18:29:12 +0100

    added Leaflet map

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./Contact.css";
 
 function Contact() {
   return (
     <div className="Contact">
       <div className="container">
         <h2>Elérhetöség</h2>
-        <p>Térkép helye</p>
+        <div className="leaflet-container">
+          <MapContainer center={[47.379789, 16.786528]} zoom={17}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[47.379789, 16.786528]}>
+              <Popup>Thermál delikát</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
         <div className="row">
           <div className="col-6">
             <p>email cim</p>
