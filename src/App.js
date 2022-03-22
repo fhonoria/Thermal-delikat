bee04500a036cdc8dd8d04badf218cc9741e2111
fhonoria <83973848+fhonoria@users.noreply.github.com>
2022-03-22 15:01:27 +0100

    removed navbar temporarily

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import "./App.css";
 import "./components/Introduction";
-import Navbar from "./components/Navbar";
 import Introduction from "./components/Introduction";
 import Team from "./components/Team";
 import Products from "./components/Products";
 import Contact from "./components/Contact";
 import BackgroundImage from "./components/BackgroundImage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-
       <BackgroundImage />
       <div className="container titel text-center">
         <h1>THERMÁL DELIKÁT</h1>
